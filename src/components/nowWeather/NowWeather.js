bde1239a6e75935f09a97b3f437e52e253872be6
Alejandro-Vas <90215047+Alejandro-Vas@users.noreply.github.com>
2021-12-24 14:01:28 +0300

    new service

--- src/components/nowWeather/NowWeather.js
+++ src/components/nowWeather/NowWeather.js
@@ -1,16 +1,44 @@
-import GetWeatherService from "../../services/GetWeatherService";
+import { useState, useEffect } from "react";
+
+const api = {
+  key: "4a988ac25507ea7c902562b2aa291b85",
+  base: "https://api.openweathermap.org/data/2.5/weather?q="
+};
 
 const NowWeather = () => {
-  const { result } = GetWeatherService();
-  console.log("now", result);
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (event) => {
+    if (event.key === "Enter") {
+      fetch(`${api.base}${query}&units=metric&appid=${api.key}&lang=ru`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
 
   return (
     <>
-      <div>
-        <h2>{result}</h2>
+      <div className="search-box">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+        />
       </div>
 
-      <div>Город: Киров {result}</div>
+      <div>
+        <h2></h2>
+      </div>
+
+      <div>Город: Киров </div>
       <div>Страна: Россия</div>
       <div>Облачность: 100% небольшой снег </div>
       <div>Температура: -16°</div>
