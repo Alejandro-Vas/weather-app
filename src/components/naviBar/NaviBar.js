37f61737671113e741ddfbe370b6c77c18ea1d15
Alejandro-Vas <zended@yandex.ru>
2021-12-26 02:04:19 +0300

    MainPage

--- src/components/naviBar/NaviBar.js
+++ src/components/naviBar/NaviBar.js
@@ -8,7 +8,7 @@ const NaviBar = () => {
     <div className="navi-bar-wrapper shadow-lg mb-4 bg-white">
       <Navbar bg="primary" variant="dark">
         <Container>
-          <Nav className=" me-auto fs-5 fw-bold ">
+          <Nav className=" me-auto fs-3 fw-bold ">
             <Nav.Link as={Link} to="/">
               Погода сейчас
             </Nav.Link>
---
+++ src/pages/MainPage.js
@@ -0,0 +1,11 @@
+import NowWeather from "../components/nowWeather/NowWeather";
+
+const MainPage = () => {
+  return (
+    <>
+      <NowWeather />
+    </>
+  );
+};
+
+export default MainPage;
