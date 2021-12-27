e317e86c0d20aac529724c34e66be24dd894a723
Alejandro-Vas <90215047+Alejandro-Vas@users.noreply.github.com>
2021-12-27 16:11:05 +0300

    logo cdn

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,15 @@
-import logo from "../../resourses/img/logo-min-80px.png";
+// import logo from "../../resourses/img/logo-min-80px.png";
 
 import "./Header.scss";
 
 const Header = () => {
+  const logoCdn =
+    "https://rawcdn.githack.com/Alejandro-Vas/weather-app/10827780f269d8862b6f5a34a112d4463d8521d3/src/resourses/img/logo-min-80px.png";
   return (
     <>
       <div className="logo-wrapper">
         <div className="header-img">
-          <img src={logo} alt="Weatherman App logo" />
+          <img src={logoCdn} alt="Weatherman App logo" />
         </div>
         <div className="header-text">
           <h1>Weathermann</h1>
