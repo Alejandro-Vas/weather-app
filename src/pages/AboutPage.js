4b9e00959c3dcb658867474e5b69df140e21d5c6
Alejandro-Vas <90215047+Alejandro-Vas@users.noreply.github.com>
2021-12-30 11:39:14 +0300

    about scss

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,4 +1,5 @@
 import githubLogo from "../resourses/img/GitHub-Mark-32px.png";
+import "./AboutPage.scss";
 
 const AboutPage = () => {
   return (
@@ -6,13 +7,21 @@ const AboutPage = () => {
       <div>Спасибо, что пользуетесь данным сервисом.</div>
       <div>
         Пожелания по развитию проекта можно направлять:
-        <div>
-          <a href="https://github.com/Alejandro-Vas/weather-app">
-            <img src={githubLogo} alt="gitHub repository" />
-          </a>
-          <a href="https://github.com/Alejandro-Vas/weather-app">
-            GitHub Repository
-          </a>
+        <div className="repo-link">
+          <div className="repo-link--item">
+            <a href="https://github.com/Alejandro-Vas/weather-app">
+              <img src={githubLogo} alt="gitHub repository" />
+            </a>
+          </div>
+
+          <div className="repo-link--item">
+            <a
+              className="repo-link"
+              href="https://github.com/Alejandro-Vas/weather-app"
+            >
+              GitHub Repository
+            </a>
+          </div>
         </div>
       </div>
     </>
