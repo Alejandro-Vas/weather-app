a683540e664d228ea0f96a7232c27f21150f259f
Alejandro-Vas <90215047+Alejandro-Vas@users.noreply.github.com>
2021-12-30 11:07:05 +0300

    github href

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,12 +1,17 @@
+import githubLogo from "../resourses/img/GitHub-Mark-32px.png";
+
 const AboutPage = () => {
   return (
     <>
-      <div>Спасибо, что зашли на эту страницу.</div>
+      <div>Спасибо, что пользуетесь данным сервисом.</div>
       <div>
-        Пожелания по развитию проекта можно направлять
+        Пожелания по развитию проекта можно направлять:
         <div>
           <a href="https://github.com/Alejandro-Vas/weather-app">
-            https://github.com/Alejandro-Vas/weather-app
+            <img src={githubLogo} alt="gitHub repository" />
+          </a>
+          <a href="https://github.com/Alejandro-Vas/weather-app">
+            GitHub Repository
           </a>
         </div>
       </div>
