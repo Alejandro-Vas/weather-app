6d1126604436cd794e36ccf3384fa2a37e11350f
Alejandro-Vas <90215047+Alejandro-Vas@users.noreply.github.com>
2021-12-30 14:21:30 +0300

    lat lon fetch

--- src/services/getForecastWeather.js
+++ src/services/getForecastWeather.js
@@ -1,9 +1,10 @@
-function getCurrentWeather(query) {
+function getCurrentWeather(lat, lon) {
   const api = {
     key: "4a988ac25507ea7c902562b2aa291b85",
-    base: "https://api.openweathermap.org/data/2.5/weather?q=",
+    base: "https://api.openweathermap.org/data/2.5/onecall?"
   };
-  return fetch(`${api.base}${query}&units=metric&appid=${api.key}&lang=ru`);
+  console.log(`${api.base}lat=${lat}&lon=${lon}&appid=${api.key}&lang=ru`);
+  return fetch(`${api.base}lat=${lat}&lon=${lon}&appid=${api.key}&lang=ru`);
 }
 
 export default getCurrentWeather;
