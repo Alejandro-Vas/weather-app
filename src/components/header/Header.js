b78c4e32f979ddaec25863ac320ea84f1b6114ae
Alejandro-Vas <zended@yandex.ru>
2022-01-03 01:29:17 +0300

    logo href

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,24 @@
 // import logo from "../../resourses/img/logo-min-80px.png";
+import { Link } from "react-router-dom";
 
 import "./Header.scss";
 
 const Header = () => {
-  const logoCdn =
+  const logoCDN =
     "https://rawcdn.githack.com/Alejandro-Vas/weather-app/10827780f269d8862b6f5a34a112d4463d8521d3/src/resourses/img/logo-min-80px.png";
   return (
     <>
       <div className="header-wrapper">
-        <div className="header-img-wrapper">
-          <img className="header-img-logo" src={logoCdn} alt="Weather App" />
-        </div>
+        <Link to="/">
+          <div className="header-wrapper__img">
+            <img
+              className="header-wrapper__logo"
+              src={logoCDN}
+              alt="Weather App"
+            />
+          </div>
+        </Link>
+
         <div className="header-text">
           <h1>Weathermann</h1>
         </div>
