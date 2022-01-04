aa0a33834d6d9564fc67d654622d5bdd799c0deb
Alejandro-Vas <zended@yandex.ru>
2022-01-04 16:32:37 +0300

    metric units api call

--- src/services/getForecastWeather.js
+++ src/services/getForecastWeather.js
@@ -4,9 +4,13 @@ function getForecastWeather(coordiantes) {
     key: "4a988ac25507ea7c902562b2aa291b85",
     base: "https://api.openweathermap.org/data/2.5/onecall?",
   };
-  console.log(`${api.base}lat=${lat}&lon=${lon}&appid=${api.key}&lang=ru`);
+  console.log(
+    `${api.base}lat=${lat}&lon=${lon}&appid=${api.key}&lang=ru&units=metric`
+  );
 
-  return fetch(`${api.base}lat=${lat}&lon=${lon}&appid=${api.key}&lang=ru`);
+  return fetch(
+    `${api.base}lat=${lat}&lon=${lon}&appid=${api.key}&lang=ru&units=metric`
+  );
 }
 
 export default getForecastWeather;
