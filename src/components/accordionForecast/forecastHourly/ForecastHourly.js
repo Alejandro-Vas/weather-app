a52087d8167dee8882ad5e8d300a2c97c49de125
Alejandro-Vas <zended@yandex.ru>
2022-01-05 16:11:35 +0300

    forecast flex

--- src/components/accordionForecast/forecastHourly/ForecastHourly.js
+++ src/components/accordionForecast/forecastHourly/ForecastHourly.js
@@ -2,6 +2,8 @@ import WeatherIcon from "../../weatherIcon/WeatherIcon";
 import unixTimeToLocal from "./../../../functions/unixTimetoLocal";
 import getWindDirection from "./../../../functions/getWindDirection";
 
+import "./ForecastHourly.scss";
+
 const ForecastHourly = (props) => {
   const {
     forecast: { hourly },
@@ -10,16 +12,25 @@ const ForecastHourly = (props) => {
     <>
       {hourly.map((el, index) => {
         return index > 0 && index < 12 ? (
-          <div key={index}>
-            <WeatherIcon icon={el.weather[0].icon} />
-            <div>{unixTimeToLocal(el.dt, true)}</div>
-            <div>
-              температура {Math.round(el.temp)}°, ощущается как {el.feels_like}
-              °, ветер {el.wind_speed} м/с, {getWindDirection(el.wind_deg)},{" "}
-              {el.weather[0].description}
+          <>
+            <div key={index} className="d-flex align-items-center">
+              <div>
+                <h3>{unixTimeToLocal(el.dt, true)}</h3>
+              </div>
+              <WeatherIcon icon={el.weather[0].icon} />
+              <div className="forecast-wrapper">
+                <div>
+                  температура {Math.round(el.temp)}°, ощущается как{" "}
+                  {el.feels_like}°
+                </div>
+                <div>
+                  ветер {el.wind_speed} м/с, {getWindDirection(el.wind_deg)},{" "}
+                  {el.weather[0].description}
+                </div>
+              </div>
             </div>
             <hr />
-          </div>
+          </>
         ) : null;
       })}
     </>
