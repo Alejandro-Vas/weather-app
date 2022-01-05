6c920ac81f31961e36e6c27c4742eb21f169bf66
Alejandro-Vas <zended@yandex.ru>
2022-01-06 01:45:30 +0300

    spinner center

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -3,7 +3,11 @@ import "./Spinner.scss";
 const Spinner = ({ color }) => {
   return (
     <div className="spinner">
-      <div className={`spinner-border text-${color}`} role="status"></div>
+      <div
+        className={`spinner__icon spinner-border text-${color}`}
+        role="status"
+      ></div>
+      <div className="spinner__text">загрузка...</div>
     </div>
   );
 };
