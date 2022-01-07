dfe90949307487fb3f38c9a678712d199c199d6c
Alejandro-Vas <zended@yandex.ru>
2022-01-08 00:15:12 +0300

    wind direction

--- src/functions/getWindDirection.js
+++ src/functions/getWindDirection.js
@@ -1,15 +1,6 @@
 // Function takes wind direction in degrees, returns wind direction with arrow and letter
 
 export default function getWindDirection(angle) {
-  const directions = [
-    "↓ С",
-    "↙ С-В",
-    "← В",
-    " Ю-В",
-    "↑ Ю",
-    "↗ Ю-З",
-    "→ З",
-    "↘ С-З",
-  ];
+  const directions = ["С", "С-В", "В", "Ю-В", "Ю", "Ю-З", "З", "С-З"];
   return directions[Math.round(angle / 45) % 8];
 }
