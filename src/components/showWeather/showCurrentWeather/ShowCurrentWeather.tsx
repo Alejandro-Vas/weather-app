6d97a404b4a70e3a1f8832dc070fe491883d6e2b
Alejandro-Vas <zended@yandex.ru>
2022-01-26 22:29:43 +0300

    refactor: ShowCurrentWeather split to MainPage

--- src/components/showWeather/showCurrentWeather/ShowCurrentWeather.tsx
+++ src/components/showWeather/showCurrentWeather/ShowCurrentWeather.tsx
@@ -1,86 +1,13 @@
-import { useState, useCallback } from "react";
-import SearchBox from "../../searchBox/SearchBox";
-import Spinner from "../../spinner/Spinner";
 import WeatherIcon from "../../weatherIcon/WeatherIcon";
-import ButtonSubmit from "../../buttonSubmit/ButtonSubmit";
-import { CityNotFound, LoadingError } from "../../errors/Errors";
 
 import unixTimeToLocal from "../../../functions/unixTimeToLocal";
 import getWindDirection from "../../../functions/getWindDirection";
-import getCurrentWeather from "../../../services/getCurrentWeather";
 
 import "./ShowCurrentWeather.scss";
 
-import { IProps } from "../../../pages/MainPage";
+import { IWeather } from "../../../interfaces/IWeather";
 
-const ShowCurrentWeather: React.FC<IProps> = (props) => {
-  const { query, setQuery, setCoordinates, weather, setWeather } = props;
-
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const onClearSearch = () => {
-    setQuery("");
-    setWeather({});
-    setCoordinates([]);
-  };
-
-  const clearError = useCallback(() => setError(false), []);
-
-  const onSearch = async (event) => {
-    setLoading(true);
-    event.preventDefault();
-    const result = await getCurrentWeather(query)
-      .then((result) => result.json())
-      .then((result) => {
-        setLoading(false);
-        setError(false);
-        setWeather(result);
-        setQuery(query);
-        setCoordinates([result.coord.lat, result.coord.lon]);
-
-        // setLoading(true);
-        clearError();
-      })
-      .then();
-    // .catch(setError(true));
-
-    return result;
-  };
-
-  return (
-    <div className="fade-in">
-      <SearchBox
-        query={query}
-        setQuery={setQuery}
-        onSearch={onSearch}
-        loading={loading}
-      />
-
-      {query && (
-        <ButtonSubmit
-          onClick={onClearSearch}
-          btnText="Очистить"
-          variant="danger"
-        />
-      )}
-
-      {loading && <Spinner />}
-
-      {weather.message === "city not found" ? <CityNotFound /> : null}
-
-      {!loading && error && <LoadingError />}
-
-      {!loading && typeof weather.sys !== "undefined" ? (
-        <View weather={weather} />
-      ) : null}
-    </div>
-  );
-};
-
-export default ShowCurrentWeather;
-
-const View = ({ weather }) => {
+const ShowCurrentWeather: React.FC = ({ weather }: IWeather) => {
   return (
     <div className="fade-in shadow-lg p-3 mb-5 rounded">
       <div className="city-header">
@@ -123,3 +50,5 @@ const View = ({ weather }) => {
     </div>
   );
 };
+
+export default ShowCurrentWeather;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,15 @@
+import { useState, useCallback } from "react";
+
 import ShowCurrentWeather from "../components/showWeather/showCurrentWeather/ShowCurrentWeather";
 import ErrorBoundary from "../components/errorBoundary/ErrorBoundary";
 import { IWeather } from "../interfaces/IWeather";
+
+import SearchBox from "../components/searchBox/SearchBox";
+import Spinner from "../components/spinner/Spinner";
+import ButtonSubmit from "../components/buttonSubmit/ButtonSubmit";
+import { CityNotFound, LoadingError } from "../components/errors/Errors";
+
+import getCurrentWeather from "../services/getCurrentWeather";
 
 export interface IProps {
   weather: IWeather;
@@ -12,12 +21,69 @@ export interface IProps {
 }
 
 const MainPage: React.FC<IProps> = (props) => {
+  const { query, setQuery, setCoordinates, weather, setWeather } = props;
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onClearSearch = () => {
+    setQuery("");
+    setWeather({});
+    setCoordinates([]);
+  };
+
+  const clearError = useCallback(() => setError(false), []);
+
+  const onSearch = async (event) => {
+    setLoading(true);
+    event.preventDefault();
+    const result = await getCurrentWeather(query)
+      .then((result) => result.json())
+      .then((result) => {
+        setLoading(false);
+        setError(false);
+        setWeather(result);
+        setQuery(query);
+        setCoordinates([result.coord.lat, result.coord.lon]);
+
+        // setLoading(true);
+        clearError();
+      })
+      .then();
+    // .catch(setError(true));
+
+    return result;
+  };
+
   return (
-    <>
+    <div className="fade-in">
+      <SearchBox
+        query={query}
+        setQuery={setQuery}
+        onSearch={onSearch}
+        loading={loading}
+      />
+
+      {query && (
+        <ButtonSubmit
+          onClick={onClearSearch}
+          btnText="Очистить"
+          variant="danger"
+        />
+      )}
+
+      {loading && <Spinner />}
+
+      {weather.message === "city not found" ? <CityNotFound /> : null}
+
+      {!loading && error && <LoadingError />}
       {/* <ErrorBoundary> */}
-      <ShowCurrentWeather {...props} />
-      {/* </ErrorBoundary> */}
-    </>
+
+      {!loading && typeof weather.sys !== "undefined" ? (
+        <ShowCurrentWeather weather={weather} />
+      ) : null}
+      {/* <ErrorBoundary/> */}
+    </div>
   );
 };
 
