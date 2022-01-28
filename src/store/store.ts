ec05a42414c6dd2f17bfc7684c51064a642994f6
Alejandro-Vas <zended@yandex.ru>
2022-01-28 13:29:36 +0300

    store added query reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import querySlice from "./query/querySlice";
 import { weatherApi } from "./weather/weather-api";
 
 export const store = configureStore({
-  reducer: { [weatherApi.reducerPath]: weatherApi.reducer },
+  reducer: {
+    [weatherApi.reducerPath]: weatherApi.reducer,
+    query: querySlice.reducer,
+  },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(weatherApi.middleware),
 });
