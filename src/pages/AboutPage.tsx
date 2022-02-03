ba3f03060632c69de45e09c7386abf78274e3e4f
Alejandro-Vas <zended@yandex.ru>
2022-02-04 00:19:52 +0300

    AboutPage text

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -6,21 +6,19 @@ import "./AboutPage.scss";
 const AboutPage: React.FC = () => {
   return (
     <div className="fade-in shadow-lg p-3 mb-5 rounded">
+      <div className="repo-link">
+        <div className="repo-link__item">
+          <a href="https://github.com/Alejandro-Vas/weather-app">
+            <ButtonSubmit btnText="Alejandro-VAS" variant="warning">
+              <img src={githubLogo} alt="gitHub repository" />
+            </ButtonSubmit>
+          </a>
+        </div>
+      </div>
+
       <div>
         Источник данных{" "}
         <a href="https://openweathermap.org/">openweathermap.org</a>
-      </div>
-      <div>
-        Пожелания по развитию проекта можно направлять:
-        <div className="repo-link">
-          <div className="repo-link__item">
-            <a href="https://github.com/Alejandro-Vas/weather-app">
-              <ButtonSubmit btnText="Перейти на GitHub" variant="warning">
-                <img src={githubLogo} alt="gitHub repository" />
-              </ButtonSubmit>
-            </a>
-          </div>
-        </div>
       </div>
     </div>
   );
